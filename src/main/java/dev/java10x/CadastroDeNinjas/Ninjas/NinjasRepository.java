2c48a8daafc0f1df5889dcc9a70c1062e6d179ba
Matheus <matheushenriquevitorino@gmail.com>
2026-08-19 12:28:43 -0300

    feat: Feito o NinjaRepository

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjasRepository.java
@@ -0,0 +1,7 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjasRepository extends JpaRepository<MissoesModel,Long> {
+}
