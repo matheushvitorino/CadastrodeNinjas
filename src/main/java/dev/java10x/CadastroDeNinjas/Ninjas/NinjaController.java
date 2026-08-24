60521ed0cafd831695d0019030a4e3014a8e4a6c
Matheus <matheushenriquevitorino@gmail.com>
2026-08-24 17:20:37 -0300

    feat: adiciona rotas ao ninjacontroler

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,4 +10,22 @@ public class NinjaController {
     public String boasVindas (){
          return "Essda é minha primeir amensagem nessa rota";
     }
+
+    @GetMappin("/todos")
+    public String mostrarTodosNinja(){
+        return "Todos";
+    }
+
+
+    @PutMapping("/alteraID")
+        public String alterarNinjaporID() {
+            return "Alterar ninja por id";
+        }
+
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaporID() {
+        return "Deletar ninja por id";
+    }
+
+    @ReadMapping
 }
