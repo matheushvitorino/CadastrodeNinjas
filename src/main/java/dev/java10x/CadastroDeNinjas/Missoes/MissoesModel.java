ab5d20471787360c6c822ca6d3294f48986cbb5a
Matheus <matheushenriquevitorino@gmail.com>
2026-08-26 01:07:51 -0300

    fix: corrigido bug de serialização jsonignore

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,4 +1,5 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -20,6 +21,8 @@ public class MissoesModel {
     private long id;
     private String nome;
     private String dificuldade;
+
+    @JsonIgnore
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninja;
 
