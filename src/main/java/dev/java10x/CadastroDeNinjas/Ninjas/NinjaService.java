f137b613468bfb9a1afc227890b9c105f3f8200f
Matheus <matheushenriquevitorino@gmail.com>
2026-08-26 01:01:12 -0300

    feat: Adiciona o ninja Service

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,4 +1,18 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class NinjaService {
+
+    private NinjasRepository ninjasRepository;
+
+    public NinjaService(NinjasRepository ninjasRepository) {
+        this.ninjasRepository = ninjasRepository;
+    }
+
+    public List<NinjaModel> listarNinjas(){
+        return ninjasRepository.findAll();
+    }
 }
