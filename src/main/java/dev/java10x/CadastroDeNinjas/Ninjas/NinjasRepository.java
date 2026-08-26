511e27cf2cfac87c8c0ccb08cb962fdc0b538e0f
Matheus <matheushenriquevitorino@gmail.com>
2026-08-26 01:02:38 -0300

    fix: corrigindo erro da extensao do jpa

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjasRepository.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjasRepository.java
@@ -3,5 +3,5 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NinjasRepository extends JpaRepository<MissoesModel,Long> {
+public interface NinjasRepository extends JpaRepository<NinjaModel,Long> {
 }
